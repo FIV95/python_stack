49e6b3d261f6298eb64581b30e08d45ef2d28fa5
Frankie Lawrence <frank4law@gmail.com>
2023-10-21 21:34:36 -0400

    intial commit for loops.py

---
+++ fundamentals/fundamentals/platformFollowAlongs/loops.py
@@ -0,0 +1,13 @@
+for i in range(5):
+      print(i)
+
+for e in range(1,10):
+      print(e)
+
+for n in range(0,50,5):
+      print('step', n)
+
+# negative - decreases
+
+for k in range(1000,500,-100):
+      print('loop pass', k)
