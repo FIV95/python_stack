ce9f9b05869ece542160a4395efb97affc33763c
Frankie Lawrence <frank4law@gmail.com>
2023-10-22 00:51:07 -0400

    initial commit for defaultParametersNamedArguements.py

---
+++ fundamentals/fundamentals/platformFollowAlongs/defaultParametersNamedArguments.py
@@ -0,0 +1,9 @@
+def be_chearful(name="Mr.Shadow", repeat=2):
+      print(f"Good Morning {name}\n" * repeat)
+
+be_chearful("Frankie")
+be_chearful()
+be_chearful("Benny Bob", 5)
+# OR
+be_chearful(repeat=5, name="truffles")
+be_chearful("Nan")
