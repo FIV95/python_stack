2afa781836fad28ba4b73e82e127dcb69dad752f
Frankie Lawrence <frank4law@gmail.com>
2023-10-25 18:20:34 -0400

    commit for modifcation to bankAccount.py Still have comments

--- fundamentals/oop/coreAssignments/bankAccount/bankAccount.py
+++ fundamentals/oop/coreAssignments/bankAccount/bankAccount.py
@@ -1,3 +1,5 @@
+import random
+
 class BankAccount:
 
       allAccounts = []
@@ -20,77 +22,124 @@ class BankAccount:
                   print(f'INTEREST RATE: {account.int_rate}\n')
 
 
-      def __init__ (self, first_name_on_account='', last_name_on_account='', balance=0, int_rate = .05, account_name=None):
+      def __init__ (self, first_name_on_account, last_name_on_account, balance, int_rate, accountNumber):
             self.first_name_on_account = first_name_on_account
             self.last_name_on_account = last_name_on_account
             self.int_rate = int_rate
             self.balance = balance
-            self.account_name = account_name
+            self.accountNumber = accountNumber
             BankAccount.allAccounts.append(self)
 
-      def __str__(self):
-            return self.account_name
+      def __repr__(self):
+            return f"{self.first_name_on_account} {self.last_name_on_account}'s account, Account # {self.accountNumber}\n"
 
-      def deposit(self, amount_to_deposit):
-            self.balance += amount_to_deposit
-            return self
 
-      def withdraw(self, amount_to_withdraw):
-            if BankAccount.can_withdraw(self.balance, amount_to_withdraw):
-                  self.balance -= amount_to_withdraw
-                  print(f"Your new balance after withdrawal is: {self.balance}\n")
-                  return self
-            else:
-                  print("Insufficent Funds: Charging a $5 Fee")
-                  self.balance -= 5
-                  self.balance -= amount_to_withdraw
-                  return self
-
-      # def display_account_info(self):
-      #       print(f'FIRST NAME ON ACCOUNT: {self.first_name_on_account}')
-      #       print(f'LAST NAME ON ACCOUNT: {self.last_name_on_account}')
-      #       print(f'CURRENT BALANCE: {self.balance}')
-      #       print(f'INTEREST RATE: {self.int_rate}')
+      # def deposit(self, amount_to_deposit, accountIndex):
+      #       selectedAccount = account_list[accountIndex]
+      #       self.balance += amount_to_deposit
       #       return self
+
+      # def withdraw(self, amount_to_withdraw, accountIndex):
+      #       if BankAccount.can_withdraw(self.balance, amount_to_withdraw):
+      #             self.balance -= amount_to_withdraw
+      #             print(f"Your new balance after withdrawal is: {self.balance}\n")
+      #             return self
+      #       else:
+      #             print("Insufficent Funds: Charging a $5 Fee")
+      #             self.balance -= 5
+      #             self.balance -= amount_to_withdraw
+      #             return self
+
+      def display_account_info(self):
+            print(f'ACCOUNT NUMBER: {self.accountNumber}')
+            print(f'FIRST NAME ON ACCOUNT: {self.first_name_on_account}')
+            print(f'LAST NAME ON ACCOUNT: {self.last_name_on_account}')
+            print(f'CURRENT BALANCE: {self.balance}')
+            print(f'INTEREST RATE: {self.int_rate}\n')
+            return self
 
       def yield_interest(self):
             self.balance *= self.int_rate
             return self
 
-account1 = BankAccount(first_name_on_account='Alan' ,balance=5000, int_rate=.02)
-account2 = BankAccount(first_name_on_account='Alan',balance=500, int_rate=.02)
-account3 = BankAccount()
-account4 = BankAccount()
-account1.deposit(500).deposit(500).deposit(500).withdraw(5499)
-account2.deposit(150).deposit(100).withdraw(100).withdraw(150).withdraw(400).withdraw(50).yield_interest()
 
-# account1.printAllAccountData()
+class User(BankAccount):
 
-class User:
-
-      def __init__(self, name, email, account):
-            self.name = name
+      def __init__(self, fName, lName, email):
+            self.fName = fName
+            self.lName = lName
             self.email = email
-            self.account = account
             self.account_list = []
 
-      def create_account(self, int_rate=.02, balance=0):
-            new_account = BankAccount(int_rate=int_rate,balance=balance)
-            self.account_list.append(new_account)
+      def create_account(self,fName, lName, int_rate, balance): ## THIS METHOD WILL ALLOW USERS TO CREATE BankAccounts // fName
+            accountNumber = random.randint(10230203123,1000002043005045)  ## this random number will fufill the accountNumber attribue of the bankAccount class - it will be an identifier number
+            new_account = BankAccount(fName, lName, balance, int_rate, accountNumber) ## remember the outline we made for creating a bankAccount, we need a//  first_name_on_account // last_name_on_account // int_rate // balance // accountNumber // this fufills the constructor functions parameters
+            self.account_list.append(new_account) ## this append simply adds this new account to the Users account list
             return self
 
-      def __repr__(account):
-            return ('account')
+      def deposit(self, amount_to_deposit, accountIndex):
+            selectedAccount = self.account_list[accountIndex]
+            selectedAccount.balance += amount_to_deposit
+            return selectedAccount.balance
 
-      def make_deposit(self, amount):
-            self.account.balance += amount
+      def withdraw(self, amount_to_withdraw, accountIndex):
+            selectedAccount = self.account_list[accountIndex]
+            if selectedAccount.can_withdraw(selectedAccount.balance, amount_to_withdraw):
+                  selectedAccount.balance -= amount_to_withdraw
+                  print(f"Your new balance after withdrawal is: {selectedAccount.balance}\n")
+                  return selectedAccount
+            else:
+                  print("Insufficent Funds: Charging a $5 Fee")
+                  selectedAccount.balance -= 5
+                  selectedAccount.balance -= amount_to_withdraw
+                  return selectedAccount
 
-      def make_withdrawl(self, amount):
-            self.account.balance -= amount
+      def display_account_balance(self, accountIndex):
+            selectedAccount = self.account_list[accountIndex]
+            print(f'${selectedAccount.balance} is your current balance.\n')
 
-      def display_user_balance(self):
-            print(self.account.balance)
+      def display_account_info(self, accountIndex):
+            selectedAccount = self.account_list[accountIndex]
+            selectedAccount.display_account_info()
 
-alanWake1 = User('Alan', 'alanistheman@yahoo.com', account1)
-alanWake1.create_account(account1)
-alanWake1.create_account(account2)
+alanWake1 = User('Alan', 'Wake', 'alanistheman@yahoo.com')
+alanWake1.create_account("Alan", "Wake", .02, 5000)
+alanWake1.create_account("Alan", "Wake", .02, 3000)
+# print(alanWake1.account_list[0])
+# print(alanWake1.account_list[1])
+# print(alanWake1.account_list)
+alanWake1.account_list[0].display_account_info()
+alanWake1.account_list[1].display_account_info()
+# print(alanWake1.account_list[1].display_account_info())
+alanWake1.deposit(523,0)
+alanWake1.account_list[0].display_account_info()
+alanWake1.withdraw(523,0)
+alanWake1.account_list[0].display_account_info()
+alanWake1.display_account_balance(0)
+alanWake1.display_account_info(0)
+
+
+
+
+
+
+
+
+
+# user_list = [
+#       {
+#             "first_name": "Alan",
+#             "last_name": "Wake",
+#             "Accounts": ["<account1>","<account2>" "<account3>"],
+#       },
+#             {
+#             "first_name": "Bob",
+#             "last_name": "Anders",
+#             "Accounts": ["<account1>","<account2>" "<account3>"],
+#       },
+#             {
+#             "first_name": "Conner",
+#             "last_name": "Tillman",
+#             "Accounts": ["<account1>","<account2>" "<account3>"],
+#       },
+# ]
