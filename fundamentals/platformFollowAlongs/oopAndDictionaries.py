29292a106b9653456257147304635a823086d8f9
Frankie Lawrence <frank4law@gmail.com>
2023-10-25 00:09:32 -0400

    initial commit for oopAndDictionaries.py

---
+++ fundamentals/platformFollowAlongs/oopAndDictionaries.py
@@ -0,0 +1,8 @@
+kevin = {"Name": "Kevin Durant", "age": 34, "position": "small forward", "team": "Brookyln Nets"}
+
+class Player:
+      def __init__(self,name, age, position, team):
+            self.name = kevin["Name"]
+            self.age = kevin["age"]
+            self.positon = kevin["position"]
+            self.team = kevin["team"]
