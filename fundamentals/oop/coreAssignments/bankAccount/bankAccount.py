32cd4ce5d9fac7a210a1c3de72d077e225cd5b7f
Frankie Lawrence <frank4law@gmail.com>
2023-10-24 23:44:08 -0400

    commit for adding user class

--- fundamentals/oop/coreAssignments/bankAccount/bankAccount.py
+++ fundamentals/oop/coreAssignments/bankAccount/bankAccount.py
@@ -20,13 +20,16 @@ class BankAccount:
                   print(f'INTEREST RATE: {account.int_rate}\n')
 
 
-      def __init__ (self, first_name_on_account, last_name_on_account, balance, int_rate = .05):
+      def __init__ (self, first_name_on_account='', last_name_on_account='', balance=0, int_rate = .05, account_name=None):
             self.first_name_on_account = first_name_on_account
             self.last_name_on_account = last_name_on_account
             self.int_rate = int_rate
             self.balance = balance
+            self.account_name = account_name
             BankAccount.allAccounts.append(self)
 
+      def __str__(self):
+            return self.account_name
 
       def deposit(self, amount_to_deposit):
             self.balance += amount_to_deposit
@@ -54,9 +57,40 @@ class BankAccount:
             self.balance *= self.int_rate
             return self
 
-account1 = BankAccount("Charles", "Martel", 4000)
-account2 = BankAccount("El", "Cid", 500)
+account1 = BankAccount(first_name_on_account='Alan',balance=5000, int_rate=.02)
+account2 = BankAccount(first_name_on_account='Alan',balance=500, int_rate=.02)
+account3 = BankAccount()
+account4 = BankAccount()
 account1.deposit(500).deposit(500).deposit(500).withdraw(5499)
 account2.deposit(150).deposit(100).withdraw(100).withdraw(150).withdraw(400).withdraw(50).yield_interest()
 
-account1.printAllAccountData()
+# account1.printAllAccountData()
+
+class User:
+
+      def __init__(self, name, email, account):
+            self.name = name
+            self.email = email
+            self.account = account
+            self.account_list = []
+
+      def create_account(self, int_rate=.02, balance=0):
+            new_account = BankAccount(int_rate=int_rate,balance=balance)
+            self.account_list.append(new_account)
+            return self
+
+      def __repr__(account):
+            return ('account')
+
+      def make_deposit(self, amount):
+            self.account.balance += amount
+
+      def make_withdrawl(self, amount):
+            self.account.balance -= amount
+
+      def display_user_balance(self):
+            print(self.account.balance)
+
+alanWake1 = User('Alan', 'alanistheman@yahoo.com', account1)
+alanWake1.create_account(account1)
+alanWake1.create_account(account2)
