744d7edc7d4dc63a0052d589bd8a0e81e00aa31e
Frankie Lawrence <frank4law@gmail.com>
2023-10-25 21:36:14 -0400

    commit for basketBallDictionaries updated task completed

--- fundamentals/oop/practiceAssignments/basketballDictionaries/basketballDictionaries.py
+++ fundamentals/oop/practiceAssignments/basketballDictionaries/basketballDictionaries.py
@@ -1,66 +1,106 @@
-players = [
-      {
-      "name": "Kevin Durant",
-      "age": "34",
-      "position": "small forward",
-      "team": "Brookyln Nets"
-      },
-      {
-      "name": "Jason Tatum",
-      "age": "24",
-      "position": "small forward",
-      "team": "Boston Celtics"
-      },
-      {
-            "name": "Kyrie Irving",
-      "age": "32",
-      "position": "Point Guard",
-      "team": "Brookyln Nets"
-      },
-      {
-            "name": "Damien Lillard",
-      "age": "33",
-      "position": "Point Guard",
-      "team": "Portland Trailblazers"
-      },
-      {
-            "name": "Joel Embiid",
-      "age": "32",
-      "position": "power forward",
-      "team": "Philly 76ers"
-      },
-      {
-            "name": "DeMar DeRozan",
-      "age": "32",
-      "position": "shooting guard",
-      "team": "Chicago Bulls"
-      }
+players_data = [
+    {
+        "name": "Kevin Durant",
+        "age": "34",
+        "position": "small forward",
+        "team": "Brookyln Nets",
+    },
+    {
+        "name": "Jason Tatum",
+        "age": "24",
+        "position": "small forward",
+        "team": "Boston Celtics",
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": "32",
+        "position": "Point Guard",
+        "team": "Brookyln Nets",
+    },
+    {
+        "name": "Damien Lillard",
+        "age": "33",
+        "position": "Point Guard",
+        "team": "Portland Trailblazers",
+    },
+    {
+        "name": "Joel Embiid",
+        "age": "32",
+        "position": "power forward",
+        "team": "Philly 76ers",
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": "32",
+        "position": "shooting guard",
+        "team": "Chicago Bulls",
+    },
 ]
+
+new_team = []
+# for data in players_data:
+#     player = {}  # Create an empty dictionary for the player
+#     player.update(data)  # Update the empty dictionary with the player's information
+#     new_team.append(player)  # Add the updated dictionary to the players list
+
+
+# KevinDurant = [0]
+# JasonTatum = [1]
+# KyrieIrving = [2]
+# DamienLillard = [3]
+# JoelEmbiid = [4]
+# DemarDeRozan = [5]
+# print(players)
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets",
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics",
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets",
+}
+
+# print(kevin["name"])
+# Create your Player instances here!
+# player_jason = ???
+
+
+
 class Player:
 
       @classmethod
-      def printAllPlayerData(cls):
-            for i in players:
-                  print(i["name"])
-                  print(i["age"])
-                  print(i["position"])
-                  print(i["team"])
+      def teamCreator(cls):
+            new_team = []
+            for player_data in players_data:
+                  player = Player(player_data)
+                  new_team.append(player)
+            print(f'The New Team list: {new_team}')
 
-      @classmethod
-      def createInstances(cls):
-                  playerInstanceList = []
-                  for i in players:
-                   newplayerinstance = Player((i["name"]), (i["age"]), (i["position"]), (i["team"]))
-                   playerInstanceList.append(newplayerinstance)
-                   print(playerInstanceList)
+      def __init__(self, player_dict):
+            self.name = player_dict["name"]
+            self.age = player_dict["age"]
+            self.position = player_dict["position"]
+            self.team = player_dict["team"]
+
+      def __repr__(self):
+            return f"Player: {self.name}, Age: {self.age}, Position: {self.position}, Team: {self.team}"
 
 
 
-      def __init__(self, player_info):
-            self.name = player_info["name"]
-            self.age = player_info["age"]
-            self.position = player_info["position"]
-            self.team = player_info["team"]
-
-Player.printAllPlayerData()
-Player.createInstances()
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
+player_jason = Player(jason)
+print(player_kyrie)
+print(player_kevin)
+player_kevin.teamCreator()
