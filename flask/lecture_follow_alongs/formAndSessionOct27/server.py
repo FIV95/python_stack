69179d7f69d354bc75935f15c4e4b38075dbbea8
Frankie Lawrence <frank4law@gmail.com>
2023-10-27 13:16:55 -0400

    initial commit for formAndSession

---
+++ flask/lecture_follow_alongs/formAndSessionOct27/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+def charge_card(card_number):
+      print(f'charging {card_number} $200.00...')
+
+@app.route('/welcome')
+def main():
+      return render_template("index.html")
+
+@app.route('/welcome')
+def welcome():
+      return redirect('/welcome')
+
+@app.route('/buy')
+def buy():
+      return render_template("form.html")
+
+
+# this route returns the form
+@app.route('/accept_form', methods=["POST"])
+def accept():
+      print(f"Name {request.form['user_name']} ")
+      print(f"Qty: {request.form['qty']} ")
+      print(f"Card: {request.form['user_card']} ")
+
+      request_copy = {
+            **request.form
+      }
+
+      print(request.form)
+      ###
+
+      ###
+      charge_card(request.form['user_card'])
+      return render_template("thanks.html", name=request.form['user_name'])
+
+@app.route('/thanks')
+
+
+
+
+app.run(debug=True, host="localhost", port=8000)
